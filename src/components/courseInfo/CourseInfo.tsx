b0c5329d05fe9d4ac3092eeb8af6403236bfa8f1
Yousef Na <yousefnasr0541@gmail.com>
2025-10-18 03:52:18 +0300

    fix: imports

--- src/components/courseInfo/CourseInfo.tsx
+++ src/components/courseInfo/CourseInfo.tsx
@@ -5,7 +5,22 @@ import { FaChalkboardTeacher, FaBookReader } from 'react-icons/fa';
 import { FaRegClock } from 'react-icons/fa6';
 import { RiBookShelfLine } from 'react-icons/ri';
 import { GrLanguage } from 'react-icons/gr';
-import CourseInfoItem from './CourseInfoItem';
+
+const CourseInfoItem: React.FC<CourseInfoItemProps> = function ({
+  icon,
+  title,
+  value,
+}) {
+  return (
+    <div className="py-5 flex justify-between items-center w-full">
+      <div className="flex items-center gap-2">
+        {icon}
+        <span className="text-lg font-medium text-gray-700">{title}:</span>
+      </div>
+      <span className="text-lg font-medium text-gray-700">{value}</span>
+    </div>
+  );
+};
 
 function CourseInfo() {
   const CourseInfoItems: CourseInfoItemProps[] = [
